a8571f5e9f1a5e68537f8c938e7986e466f5627a
Kelly Dingman <kelly@Kellys-MacBook-Pro.local>
2022-03-04 16:51:28 -0500

    index associations created

--- models/index.js
+++ models/index.js
@@ -1,16 +1,29 @@
-// import models
+// Import Models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Create Associations between Models
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  forieignKey: 'category_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,
